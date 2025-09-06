1bcf6e3fa51fd6d254a4079675159450f10163f0
vantika <vantika2000@gmail.com>
2025-09-07 03:40:28 +0530

    send file response

--- myApp.js
+++ myApp.js
@@ -2,10 +2,14 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-app.get("/",function(req, res) {
-    res.send('Hello Express');
-})
+// app.get("/",function(req, res) {
+//     res.send('Hello Express');
+// })
+absolutePath = __dirname + '/views/index.html'
 
+app.get("/",function(req, res) {
+    res.sendFile(absolutePath);
+})
 
 
 
