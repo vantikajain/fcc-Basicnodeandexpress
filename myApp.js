453443976dcaa6d3f22a9e28207222d89269e6bd
vantika <vantika2000@gmail.com>
2025-09-08 01:54:57 +0530

    jsonresponse

--- myApp.js
+++ myApp.js
@@ -13,6 +13,12 @@ app.get("/",function(req, res) {
 
 app.use("/public",express.static(__dirname + "/public"));
 
+app.get("/json",function(req,res){
+    res.json(
+        {"message": "Hello json"} 
+    )
+})
+
 
 
 
