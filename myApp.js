9f68830955472f068d4bbac18611730f76ad2502
vantika <vantika2000@gmail.com>
2025-09-09 00:05:35 +0530

    loggermiddleware

--- myApp.js
+++ myApp.js
@@ -21,17 +21,26 @@ app.use("/public",express.static(__dirname + "/public"));
 // })
 
 
-app.get("/json",function(req,res){
+// app.get("/json",function(req,res){
 
-    let message = "Hello json";
-    if(process.env.MESSAGE_STYLE=="uppercase"){
-        message=message.toUpperCase();
+//     let message = "Hello json";
+//     if(process.env.MESSAGE_STYLE=="uppercase"){
+//         message=message.toUpperCase();
         
-    }
-    res.json(
-        {"message": message} 
-    );
+//     }
+//     res.json(
+//         {"message": message} 
+//     );
+// })
+
+
+app.use("/",(req, res, next) => {
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
 })
+
+
+
 
 
 
