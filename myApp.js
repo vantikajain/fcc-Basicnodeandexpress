ca02a3ff967d874bc7fd567bedf83b8e66280efb
vantika <vantika2000@gmail.com>
2025-09-08 23:41:36 +0530

    env_variable1

--- myApp.js
+++ myApp.js
@@ -24,11 +24,12 @@ app.use("/public",express.static(__dirname + "/public"));
 app.get("/json",function(req,res){
 
     let message = "Hello json";
-    if(process.env.MESSAGE_STYLE==="uppercase"){
+    if(process.env.MESSAGE_STYLE=="uppercase"){
         message=message.toUpperCase();
+        
     }
     res.json(
-        {message: message} 
+        {"message": message} 
     );
 })
 
